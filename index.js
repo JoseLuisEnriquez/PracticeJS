7eb0b094d4e8127016083594a0b39fdced71e719
Jose Luis Enriquez <josl.enriquez89@gmail.com>
2020-10-26 19:07:49 -0300

    Radium PS04 - Exercise solution 1.a

---
+++ index.js
@@ -0,0 +1,13 @@
+// Variables y operadores
+
+/*
+1.a)
+Crear dos variables numéricas y utilizar el operador suma para guardar
+el valor de la suma de ambos números en una 3er variable.
+*/
+
+var num1, num2, res;
+num1 = 10;
+num2 = 15;
+res = num1 + num2;
+console.log('La suma de 2 números dio como resultado:', res);
