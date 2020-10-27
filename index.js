ee058a518a3beb718e2a01ff12e382ae2742c4da
Jose Luis Enriquez <josl.enriquez89@gmail.com>
2020-10-26 23:38:52 -0300

    Radium PS04 - Exercise solution 5.d

--- index.js
+++ index.js
@@ -280,7 +280,20 @@ Al final mostrar una única alerta con la cadena completa.
 
 var sentence;
 sentence = list[0];
-for (var i = 1; i <5; i++){
+for (var i = 1; i < 5; i++){
     sentence = sentence + list[i];
 }
 alert(sentence);
+
+/*
+5.d)
+Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición,
+es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array,
+desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).
+*/
+
+var listNumber = [];
+for (var i = 0; i < 10; i++){
+    listNumber[i] = i;
+}
+console.log(listNumber);
