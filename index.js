0ae3552e7d007c8fc9e9f9f0e49d0d3332534e99
Jose Luis Enriquez <josl.enriquez89@gmail.com>
2020-10-26 23:10:31 -0300

    Radium PS04 - Exercise solution 4.b

--- index.js
+++ index.js
@@ -207,3 +207,39 @@ if (nr >= 0.5){
 } else
     alert('Lower than 0,5');
 console.log(nr);
+
+/*
+4.b)
+Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+“Bebe” si la edad es menor a 2 años
+“Nino” si la edad es entre 2 y 12 años
+“Adolecente” si la edad es entre 13 y 19 años
+“Joven” si la edad está entre 20 y 30 años
+“Adulto” entre 31 y 60 años
+“Adulto mayor” entre 61 y 75 años
+“Anciano” si es mayor a 75 años
+*/
+
+var Age;
+Age = 85;
+if(Age < 2){
+    alert('Baby');
+}
+else if (Age >= 2 && Age <= 12){
+    alert('child');
+}
+else if (Age > 12 && Age <= 19){
+    alert('adolescent');
+}
+else if (Age > 19 && Age <= 30){
+    alert('young');
+}
+else if (Age > 30 && Age <= 60){
+    alert('adult');
+}
+else if (Age > 60 && Age <= 75){
+    alert('elderly');
+}
+else {
+    alert('Old man');
+}
